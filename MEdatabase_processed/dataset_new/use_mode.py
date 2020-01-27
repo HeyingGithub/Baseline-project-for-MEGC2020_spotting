85f578ea1b271d56edb20088df6d0a7679f98090
HeyingGithub <58459837+HeyingGithub@users.noreply.github.com>
2020-01-27 21:13:04 +0800

    Delete use_mode.py

--- MEdatabase_processed/dataset_new/use_mode.py
+++
@@ -1,8 +0,0 @@
-
-USE_MODE = 'all'
-
-
-
-mode = ['all', 'generate', 'use', 'evaluate']
-if USE_MODE not in mode:
-    raise Exception('Eorror! "mode" only has five options: {}.'.format(str(mode).strip('[').strip(']')))
